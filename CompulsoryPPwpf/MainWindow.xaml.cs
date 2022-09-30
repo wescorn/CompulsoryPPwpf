ee38252f5eb5ead3d65908ba47722c4ea8051544
Søren <act.wescorn@gmail.com>
2022-09-30 15:47:39 +0200

    Initial commit

---
+++ CompulsoryPPwpf/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CompulsoryPPwpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<long> primes; 
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string timeElapsed { get; set; }
+
+        private DispatcherTimer timer;
+        private Stopwatch stopWatch;
+        PrimeGenerator primegen;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // CmbScenario.Items
+            CmbScenario.Items.Add("Parallel");
+            CmbScenario.Items.Add("Sequential");
+            CmbScenario.SelectedIndex = 0;
+            RangeFrom.Text = "5";
+            RangeTo.Text = "100";
+
+            primegen = new PrimeGenerator();
+
+        }
+
+        public void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += dispatcherTimerTick_;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            timer.Start();
+        }
+
+        private void dispatcherTimerTick_(object sender, EventArgs e)
+        {
+            timeElapsed = stopWatch.Elapsed.TotalSeconds.ToString(); // Format as you wish
+            //PropertyChanged(this, new PropertyChangedEventArgs("timeElapsed"));
+
+            ElapsedTime.Text = timeElapsed;
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            long first;
+            long last;
+
+            if(RangeFrom.Text.Length != 0 && RangeTo.Text.Length != 0)
+            {
+                StartTimer();
+                first = (long)Convert.ToDouble(RangeFrom.Text);
+                last = (long)Convert.ToDouble(RangeTo.Text);
+                
+                primeListBox.ItemsSource = CmbScenario.SelectedIndex == 0 ? new ObservableCollection<long>(await primegen.GetPrimeNumbersParallelAsync(first, last)) : new ObservableCollection<long>(await primegen.GetPrimeNumbersSequentialAsync(first, last));
+
+                stopWatch.Stop();
+            }
+        }
+    }
+}
---
+++ CompulsoryPPwpf/PrimeGenerator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace CompulsoryPPwpf
+{
+    public class PrimeGenerator
+    {
+
+        Object lockMe = new Object();
+        public PrimeGenerator()
+        {
+
+        }  
+        
+
+        public List<long> GetPrimeNumbersSequential(long first, long last)
+        {
+            long num, i, ctr;
+            List<long> primes = new List<long>();
+
+            for (num = first; num <= last; num++)
+            {
+                ctr = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+
+                if (ctr == 0 && num != 1)
+                {
+                    primes.Add(num);
+                }
+
+            }
+            return primes;
+        }
+
+        public Task<List<long>> GetPrimeNumbersParallelAsync(long first, long last)
+        {
+            return Task.Run(() => GetPrimeNumbersParallel(first, last));
+        }
+
+        public Task<List<long>> GetPrimeNumbersSequentialAsync(long first, long last)
+        {
+            return Task.Run(() => GetPrimeNumbersSequential(first, last));
+        }
+
+        public List<long> GetPrimeNumbersParallel(long first, long last)
+        {
+            List<long> primes = new List<long>();
+            Parallel.ForEach(Partitioner.Create(first, last), range =>
+            {
+                Debug.WriteLine("Chunk from "+ range.Item1 + " to "+ range.Item2);
+                lock(lockMe)
+                {
+                    primes.AddRange(GetPrimeNumbersSequential(range.Item1, range.Item2 - (range.Item2 == last ? 0 : 1)));
+                }
+                
+            });
+           // primes.Sort();
+            foreach (var item in primes)
+            {
+                Debug.WriteLine(item);
+            }
+            
+            return primes;
+
+
+            //for (int iThread = 0; iThread < threads; iThread++)
+            //{
+            //    int localThread = iThread;
+            //    task[localThread] = Task.Run(() =>
+            //    {
+            //        Parallel.ForEach(Partitioner.Create(0, threadedArray.Length), range =>
+            //        {
+            //            for (int i = range.Item1; i < range.Item2; i++)
+            //            {
+            //                MathF.Sqrt(threadedArray[i]);
+            //            }
+            //        });
+            //    });
+            //}
+        }
+
+
+
+        public List<long> GetPrimesTest(long first, long last)
+        {
+            long num, i, ctr;
+            List<long> primes = new List<long>();
+
+            for (num = first; num <= last; num++)
+            {
+                ctr = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+
+                if (ctr == 0 && num != 1)
+                {
+                   
+                    primes.Add(num);
+                }
+                    
+            }
+            return primes;
+        }
+    }
+}
